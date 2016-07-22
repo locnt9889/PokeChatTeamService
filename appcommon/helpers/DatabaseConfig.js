dd880dc532ad9805aecf99dbb0f10070fd34804c
LocNT <locnt@vn.ibm.com>
2016-07-22 11:41:30 +0700

    register

--- appcommon/helpers/DatabaseConfig.js
+++ appcommon/helpers/DatabaseConfig.js
@@ -5,25 +5,25 @@
 var mysql     =    require('mysql');
 
 //localhost for disploy
-var DB_CONFIG = {
-    connectionLimit : 100, //important
-    host : 'localhost',
-    user : 'root',
-    port : '3306',
-    password : '123456',
-    database : 'poke_team_chat_db',
-    debug    :  false
-};
-
-//server for disploy
 //var DB_CONFIG = {
 //    connectionLimit : 100, //important
 //    host : 'localhost',
 //    user : 'root',
 //    port : '3306',
-//    password : 'Chomoi2015',
+//    password : '123456',
 //    database : 'poke_team_chat_db',
 //    debug    :  false
 //};
+
+//server for disploy
+var DB_CONFIG = {
+    connectionLimit : 100, //important
+    host : 'localhost',
+    user : 'root',
+    port : '3306',
+    password : 'Chomoi2015',
+    database : 'poke_team_chat_db',
+    debug    :  false
+};
 
 exports.pool = mysql.createPool(DB_CONFIG);
